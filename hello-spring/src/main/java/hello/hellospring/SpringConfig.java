167f47b08b7b29e6590faae0f0092a7d0ca5ca6d
yjin_lap <yjinlee99@naver.com>
2022-02-21 20:26:45 +0900

    데이터베이스

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,29 @@
 package hello.hellospring;
-
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
+
+    private final DataSource dataSource;
+    private final EntityManager em;
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
+
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository() {
+        return new JpaMemberRepository(em);
     }
 }
